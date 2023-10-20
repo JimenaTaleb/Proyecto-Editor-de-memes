3320f30dccda76bc9179d64a24113bd27775c4e3
Jimena Taleb <jime-taleb@hotmail.com>
2023-10-20 10:06:12 -0300

    adding btn img and btn txt events

--- JS/main.js
+++ JS/main.js
@@ -1,4 +1,26 @@
+// Para este proyecto decidi no utilizar variables, por lo tanto utilizare la funcion $ para llamar
+// a los selectores. 
+// Al no utilizar variables, comienzo con las funciones para luego continuar con los eventos.
 
+
+// FUNCIONES
+
+const $ = (selector) => document.querySelector(selector)
+
+
+// EVENTOS
+
+
+$("#btnImg").addEventListener("click", () =>{
+    $("#bothSections").classList.toggle("hidden")
+    $("#imgSection").classList.toggle("hidden")
+})
+
+$("#btnTxt").addEventListener("click", () =>{
+    $("#bothSections").classList.toggle("hidden")
+    $("#imgSection").classList.add("hidden")
+    $("#txtSection").classList.toggle("hidden")
+})
 
 
 
